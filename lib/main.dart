54d0544f8ccdd8684f779ad491b0a3781757b766
Fernando Perez <feragusper@gmail.com>
2020-01-11 13:44:37 -0300

    Bottom navigation added  (WIP) and some minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'dart:math';
 
 import 'package:cigarette_control/firebase_firestore_util.dart';
+import 'package:circular_bottom_navigation/circular_bottom_navigation.dart';
+import 'package:circular_bottom_navigation/tab_item.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(HomeScreen());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(HomeScreen());
+}
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -41,6 +47,19 @@ class _DailySmokeCounterWidgetState extends State<DailySmokeCounterWidget> with 
 
   FirebaseFirestoreUtil _firebaseFirestore = FirebaseFirestoreUtil();
   GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+
+  List<TabItem> tabItems = List.of([
+    new TabItem(Icons.home, "Daily", Colors.blue, labelStyle: TextStyle(fontWeight: FontWeight.normal)),
+    new TabItem(Icons.search, "Cumulative", Colors.orange, labelStyle: TextStyle(color: Colors.red, fontWeight: FontWeight.bold)),
+    new TabItem(Icons.layers, "Settings", Colors.red),
+    new TabItem(Icons.notifications, "Notifications", Colors.cyan),
+  ]);
+
+  CircularBottomNavigationController _navigationController;
+
+  int selectedPos = 0;
+
+  double bottomNavBarHeight = 60;
 
   @override
   void initState() {
@@ -82,12 +101,15 @@ class _DailySmokeCounterWidgetState extends State<DailySmokeCounterWidget> with 
         _counter = counter;
       });
     });
+
+    _navigationController = new CircularBottomNavigationController(selectedPos);
   }
 
   @override
   void dispose() {
-    _controller.dispose();
     super.dispose();
+    _controller.dispose();
+    _navigationController.dispose();
   }
 
   void _incrementCounter() {
@@ -112,25 +134,32 @@ class _DailySmokeCounterWidgetState extends State<DailySmokeCounterWidget> with 
             style: new TextStyle(color: Colors.white),
           ),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "Today you've smoked:",
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.display1,
-              ),
-            ],
-          ),
+        body: Stack(
+          children: <Widget>[
+            Padding(
+              child: Center(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "Today you've smoked:",
+                  ),
+                  Text(
+                    '$_counter',
+                    style: Theme.of(context).textTheme.display1,
+                  ),
+                ],
+              )),
+              padding: EdgeInsets.only(bottom: bottomNavBarHeight),
+            ),
+            Align(alignment: Alignment.bottomCenter, child: bottomNav())
+          ],
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _incrementCounter,
-          tooltip: 'I just smoked',
-          child: Icon(Icons.add),
-        ), // This trailing comma makes auto-formatting nicer for build methods.
+//        floatingActionButton: FloatingActionButton(
+//          onPressed: _incrementCounter,
+//          tooltip: 'I just smoked',
+//          child: Icon(Icons.add),
+//        ), // This trailing comma makes auto-formatting nicer for build methods.
       );
     }
   }
@@ -175,6 +204,22 @@ class _DailySmokeCounterWidgetState extends State<DailySmokeCounterWidget> with 
         color: color,
         shape: BoxShape.circle,
       ),
+    );
+  }
+
+  Widget bottomNav() {
+    return CircularBottomNavigation(
+      tabItems,
+      controller: _navigationController,
+      barHeight: bottomNavBarHeight,
+      barBackgroundColor: Colors.white,
+      animationDuration: Duration(milliseconds: 300),
+      selectedCallback: (int selectedPos) {
+        setState(() {
+          this.selectedPos = selectedPos;
+          print(_navigationController.value);
+        });
+      },
     );
   }
 }
